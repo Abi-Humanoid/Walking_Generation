24484fdce6e3d62bcf2f4b3bb703a55fc42aeaa1
Laura Harman <lrharman@student.unimelb.edu.au>
2022-04-09 15:31:57 +1000

    planning

---
+++ KinematicswCOM.py
@@ -0,0 +1,6 @@
+# Subbing in point COM for each joint in forward (or inverse) kinematics, to then calculate total COM
+
+# Calculate COM for different walking phases
+
+# Use CoM values as a trajectory 'x' -> generate ZMP via cart table model. (LIPM uses ZMP -> CoM). 
+# Do smooth generation in Matlab, work backwards to get joint angles
